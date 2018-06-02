f4b474b65860d7a6024d64ab1e8c073903e00c44
Giovanna <giovanna.perina@hotmail.com>
2018-06-02 10:05:45 -0400

    fix paths

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-  return gulp.src('./assets/sass/*.scss')
+  return gulp.src('./public/assets/sass/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./public/assets/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./assets/sass/*.scss', ['sass']);
+  gulp.watch('./public/assets/sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch']);
